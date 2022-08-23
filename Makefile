f25bd62636edd706aab65d61db3ce6295b27e2f6
Pawel Grobelniak <pawel.grobelniak@gmail.com>
2022-08-23 14:48:34 +0200

    create font

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: v
 WARN=-Wall -Wno-unused-parameter -Wno-parentheses -Wno-unused-result
 DEPS=`sdl2-config --libs --cflags` -lm
-v: main.c scancodes.h
+v: main.c scancodes.h vt52rom.h
 	cc $(WARN) -O3 -o v main.c $(DEPS)
--- main.c
+++ main.c
@@ -2,15 +2,49 @@
 #include <SDL.h>
 
 #include "scancodes.h"
+#include "vt52rom.h"
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
+#define TERM_WIDTH 80
+#define TERM_HEIGHT 24
+
+#define CHAR_WIDTH 8
+#define CHAR_HEIGHT 20
+
+#define WINDOW_WIDTH TERM_WIDTH*CHAR_WIDTH // 640
+#define WINDOW_HEIGHT TERM_HEIGHT*CHAR_HEIGHT // 480
 
 SDL_Window *window;
 SDL_Renderer *renderer;
+SDL_Texture *fonttex[128];
 
 int shift = 0;
 int ctrl = 0;
+
+void createChar(Uint32 *raster, int c) {
+    int i, j;
+    Uint8 *chr = &vt52rom[c*8];
+    memset(raster, 0, CHAR_WIDTH*CHAR_HEIGHT*sizeof(Uint32));
+    for(i = 0; i < 8; i++) {
+        for(j = 0; j < 7; j++) {
+            if(chr[i]&(0100>>j)) {
+                raster[(i*2+0)*CHAR_WIDTH + j] = 0xFF;
+                raster[(i*2+1)*CHAR_WIDTH + j] = 0xFF;
+            }
+        }
+    }
+}
+
+void createFont() {
+    int w = CHAR_WIDTH;
+    int h = CHAR_HEIGHT;
+    Uint32 *raster = malloc(w*h*sizeof(Uint32));
+    for(int i = 0; i < 128; i++) {
+        createChar(raster, i);
+        fonttex[i] = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, w, h);
+        SDL_SetTextureBlendMode(fonttex[i], SDL_BLENDMODE_ADD);
+        SDL_UpdateTexture(fonttex[i], NULL, raster, w*sizeof(Uint32));
+    }
+}
 
 void setup() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -20,10 +54,11 @@ void setup() {
     }
     SDL_SetWindowTitle(window, "Vortex");
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    createFont();
 }
 
 void keydown(SDL_Scancode scancode, int repeat) {
-    switch(scancode){
+    switch(scancode) {
         case SDL_SCANCODE_LSHIFT:
         case SDL_SCANCODE_RSHIFT: 
             shift = 1; 
@@ -43,7 +78,7 @@ void keydown(SDL_Scancode scancode, int repeat) {
 }
 
 void keyup(SDL_Scancode scancode) {
-    switch(scancode){
+    switch(scancode) {
         case SDL_SCANCODE_LSHIFT:
         case SDL_SCANCODE_RSHIFT: 
             shift = 0; 
@@ -60,7 +95,7 @@ void keyup(SDL_Scancode scancode) {
 
 void loop() {
     SDL_Event ev;
-    while(SDL_WaitEvent(&ev) >= 0){
+    while(SDL_WaitEvent(&ev) >= 0) {
         switch(ev.type){
             case SDL_QUIT:
                 return;
